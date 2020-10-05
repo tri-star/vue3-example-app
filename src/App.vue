e3cdafce50d901521a5a93cfd1f431f99ef5dffe
tristar <tristar@urban-theory.net>
2020-10-06 08:56:47 +0900

    ページ遷移時のトランジションの追加

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,9 @@
 <template>
-  <router-view />
+  <router-view v-slot="{ Component }">
+    <transition name="page">
+      <component :is="Component" />
+    </transition>
+  </router-view>
   <div id="loading-modal"></div>
 </template>
 
@@ -13,8 +17,31 @@ export default defineComponent({
 })
 </script>
 
-<style scoped>
-h1 {
-  font-family: Arial, Helvetica, sans-serif;
+<style>
+.page-enter-active {
+  animation: fadeIn 0.3s ease-in;
+  animation-delay: 0.3s;
+  opacity: 0;
+}
+
+@keyframes fadeIn {
+  from {
+    opacity: 0;
+  }
+  to {
+    opacity: 1;
+  }
+}
+
+.page-leave-active {
+  animation: fadeOut 0.3s ease-in;
+}
+
+@keyframes fadeOut {
+  from {
+  }
+  to {
+    opacity: 0;
+  }
 }
 </style>
